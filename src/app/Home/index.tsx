9781926e04f116cab21c882782d039f4483150f4
Erica Abreu <ericaabxx@gmail.com>
2025-09-03 14:57:50 -0300

    Componente Button

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -2,11 +2,15 @@
 import { View, Image } from "react-native"
 import { styles } from "./styles"
 
+import { Button } from "@/components/Button"
+
 export function Home(){
 // retorna essa funcao app
   return (
       <View style={styles.container}>
-        <Image source={require("@/assets/logo.png")}  style={styles.logo}/>
+        <Image source={require("@/assets/logo.png")} style={styles.logo}/>
+        <Button />
+
       </View>
   )
 }
